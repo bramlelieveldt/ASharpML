b27f3bf2ba98c0586345a3f965003cbf28b950eb
bramlelieveldt <bram.lelieveldt@gmail.com>
2026-04-02 10:51:57 +0200

    Finished planning

--- manimations/main.py
+++ manimations/main.py
@@ -1,20 +1,17 @@
-def main():
-    print("Hello from manimations!")
+from manim import *
+import numpy as np
 
-
-if __name__ == "__main__":
-    main()
-
-import manim 
 
 class SquareToCircle(Scene):
     def construct(self):
-        circle = Circle()  # create a circle
+        circle = Square()  # create a circle
         circle.set_fill(PINK, opacity=0.5)  # set color and transparency
 
         square = Square()  # create a square
-        square.rotate(PI / 4)  # rotate a certain amount
+        square.rotate(PI/2)  # rotate a certain amount
+
 
         self.play(Create(square))  # animate the creation of the square
         self.play(Transform(square, circle))  # interpolate the square into the circle
         self.play(FadeOut(square))  # fade out animation
+
