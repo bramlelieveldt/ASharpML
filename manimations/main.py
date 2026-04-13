f9c7e7fb2cf8305ad0fb22fce64cb43057a36439
Alexvt0123 <alexvantol@hotmail.com>
2026-04-13 09:59:09 +0200

    Addition second wave of twice wavelength and frequency at 20s runtime

--- manimations/main.py
+++ manimations/main.py
@@ -4,7 +4,7 @@ import numpy as np
 def main():
     print("Hello from manimations!")
 
-N = 1000 # Amount of particles
+N = 500 # Amount of particles
 
 class SquareToCircle(Scene):
     def construct(self):
@@ -32,7 +32,7 @@ class SquareToCircle(Scene):
             x_0 = initial_pos[0]  # Initial x position
             y_0 = initial_pos[1]  # Initial y position
             # Sine wave motion along x-axis
-            x = x_0 + A * np.sin(2*np.pi*x_0/wl - 2*np.pi*freq*t)
+            x = x_0 + A * np.sin(2*np.pi*x_0/wl - 2*np.pi*freq*t) + A * np.sin(2*np.pi*x_0/2*wl - 2*np.pi*2*freq*t)
             y = y_0
             dot.move_to([x, y, 0])
 
@@ -45,7 +45,7 @@ class SquareToCircle(Scene):
             dot.add_updater(lambda d, dt, w=wl, f=freq, A=A, pos=initial_positions[i]: sin_dot(d, dt, w, f, A, pos))
 
         # Animate the time tracker to create motion
-        self.play(time_tracker.animate.set_value(4 * PI), run_time=4, rate_func=linear)
+        self.play(time_tracker.animate.set_value(4 * PI), run_time=20, rate_func=linear)
         
         # Remove updaters to stop motion
         for dot in dots:
